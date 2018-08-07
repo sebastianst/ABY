4a21ec4636f1545c1e40ea786dac66618dd214f8
Sebastian Stammler <stammler.s@gmail.com>
2018-08-07 19:55:16 +0200

    Examples: Use string instead of char*

--- src/examples/sha1/common/sha1_circuit.cpp
+++ src/examples/sha1/common/sha1_circuit.cpp
@@ -21,7 +21,7 @@
 #include "../../../abycore/ENCRYPTO_utils/cbitvector.h"
 #include "../../../abycore/ENCRYPTO_utils/crypto/crypto.h"
 
-int32_t test_sha1_circuit(e_role role, char* address, uint16_t port, seclvl seclvl, uint32_t nvals, uint32_t nthreads, e_mt_gen_alg mt_alg, e_sharing sharing) {
+int32_t test_sha1_circuit(e_role role, const std::string& address, uint16_t port, seclvl seclvl, uint32_t nvals, uint32_t nthreads, e_mt_gen_alg mt_alg, e_sharing sharing) {
 	uint32_t bitlen = 32;
 	uint32_t sha1bits_per_party = ABY_SHA1_INPUT_BITS/2;
 	uint32_t sha1bytes_per_party = bits_in_bytes(sha1bits_per_party);
